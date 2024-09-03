23df84f757cc22700872380c31a63b1f287a183a
Hugo-Moreskh <hugo.moreskh@laplateforme.io>
2024-09-03 14:26:48 +0200

    test

---
+++ Jour02/job1/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$pdo = new PDO("mysql:host=localhost;dbname=lp_official", 'root', '');
+
+function find_all_students(): array {};
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>job1</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
